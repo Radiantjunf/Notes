91661a58d41d3fbbb071113387b7bfb2ea8ab44a
CleriaMaquiniche <130221038@estudantes.ips.pt>
2021-01-18 19:47:35 +0000

    Login

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 import Main from './app/components/Main';
+import Main from './app/screens/login';
+
+
+
+
+    const AppNavigator = createStackNavigator(
+      {
+        Home: {
+           screen: login
+           },
+           main: {
+              screen:  Main
+            }
+      },
+      {
+        initialRouteName: 'Home'
+      }
+    );
+
+    const AppContainer = createAppContainer(AppNavigator);
 
 
 export default class App extends React.Component{
-    render(){
-        return (
-          <Main />
-        );
+   render(){
+        return <AppContainer/>
+    
+        
+        
     }
-}
+  }
 
+
+
+  
